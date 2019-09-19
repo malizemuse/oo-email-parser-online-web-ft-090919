b9ba8ac80b762d4ef76f48b52de14dbb972fde66
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 19:20:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -21,7 +21,7 @@ end
 
 # TEST CODE 
 
-email_addresses = "john@doe.com, person@somewhere.org"
+puts email_addresses = "john@doe.com, person@somewhere.org"
 parser = EmailAddressParser.new(email_addresses)
  
-parser.parse
+puts parser.parse
